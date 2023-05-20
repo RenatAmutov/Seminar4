507a809000a7fd78f7faec5e7477270cfe806249
Renat Amutov <Renat.amytov@gmail.com>
2023-05-20 14:03:35 +0600

    1

---
+++ task_25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 
+
+int Deg(int a, int b){
+int result = a;
+    for(int i = 1; i < b; i++){
+        result *= a;
+    }
+        return result;
+}
+
+Console.WriteLine("Данная программа возводит числа в степень");
+
+Console.WriteLine("Введите число");
+
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень в которую будем возводить");
+
+int B = Convert.ToInt32(Console.ReadLine());
+
+int C = Deg(A,B);
+
+Console.WriteLine($"цифра {A} в степени {B} равна {C}");
